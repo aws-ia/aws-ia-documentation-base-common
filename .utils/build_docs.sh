49a157bd182661b45dc5814f1965e5ea4455c671
Andrew Glenn <29951057+andrew-glenn@users.noreply.github.com>
2022-01-19 16:55:20 -0600

    Adding preview conditional

--- .utils/build_docs.sh
+++ .utils/build_docs.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 set -e
 
-function build_deployment_guide_with_asciidoc_attributes(){
-    set +x
-    asciidoctor --base-dir docs/ --backend=html5 -o ../docs/index.html -w --doctype=book -a toc2 -a production_build docs/boilerplate/index_deployment_guide.adoc
-    set -x
-}
+# -----------
+# ${parameter:+word}
+#     If parameter is null or unset, nothing is substituted, 
+#       otherwise the expansion of word is substituted.
+#
+# This is used to add values to variables if ${BUILD_PREVIEW_GUIDE} is unset. 
+# - ex: a production build.
+#
+#
 
-function build_operational_guide_with_asciidoc_attributes(){
-    set +x
-    asciidoctor --base-dir docs/ --backend=html5 -o ../docs/index_operational.html -w --doctype=book -a toc2 -a production_build docs/boilerplate/index_operational_guide.adoc
-    set -x
-}
+PRODUCTION_ASCIIDOC_ATTRIBUTES="-a production_build"
 
-function build_migration_guide_with_asciidoc_attributes(){
+function build_guide_with_asciidoc_attributes(){
     set +x
-    asciidoctor --base-dir docs/ --backend=html5 -o ../docs/index_migration.html -w --doctype=book -a toc2 -a production_build docs/boilerplate/index_migration_guide.adoc
+    asciidoctor --base-dir docs/ --backend=html5 -o ../${OUTPUT_FILE} -w --doctype=book -a toc2 ${BUILD_PREVIEW_GUIDE:${PRODUCTION_ASCIIDOC_ATTRIBUTES}} ${LAYOUT_FILE}
     set -x
 }
 
@@ -24,15 +24,42 @@ OPERATIONALFILE=docs/_operational_guide.adoc
 MIGRATIONFILE=docs/_migration_guide.adoc
 
 if test -f "$DEPLOYMENTFILE"; then
-    build_deployment_guide_with_asciidoc_attributes
+    LAYOUT_FILE=docs/boilerplate/index_deployment_guide.adoc
+    OUTPUT_FILE=index.html
+    
+    if [[ "${BUILD_PREVIEW_GUIDE}" == "true" ]]; then
+        mkdir -p preview/
+        OUTPUT_FILE=preview/index.html
+    fi
+
+    build_guide_with_asciidoc_attributes
 fi
 
 
 if test -f "$OPERATIONALFILE"; then
-    build_operational_guide_with_asciidoc_attributes
+    mkdir -p operational/
+    LAYOUT_FILE=docs/boilerplate/index_operational_guide.adoc
+    OUTPUT_FILE=operational/index.html
+
+    if [[ "${BUILD_PREVIEW_GUIDE}" == "true" ]]; then
+        mkdir -p operational/preview/
+        OUTPUT_FILE=operational/preview/index.html
+    fi
+
+    build_guide_with_asciidoc_attributes
 fi
 
 
 if test -f "$MIGRATIONFILE"; then
-    build_migration_guide_with_asciidoc_attributes
+    mkdir -p migration/
+    LAYOUT_FILE=docs/boilerplate/index_migration_guide.adoc
+    OUTPUT_FILE=migration/index.html
+
+    if [[ "${BUILD_PREVIEW_GUIDE}" == "true" ]]; then
+        mkdir -p migration/preview/
+        OUTPUT_FILE=migration/preview/index.html
+    fi
+
+    build_guide_with_asciidoc_attributes
+
 fi
